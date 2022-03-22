f0e2c60782529b1d44c4dcc244a7299e800c6a9e
onbat2 <quangduc11202@gmail.com>
2022-03-22 16:19:15 +0700

    update

---
+++ Qt Tutorials For Beginners/LoginApp/mainwindow.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_Login_clicked()
+{
+    QString username = ui -> lineEdit_username -> text();
+    QString password = ui -> lineEdit_password -> text();
+
+    if(username == "AnhSon" && password == "Mr.JellyFish" ){
+        QMessageBox::information(this,"Login","UserName and Password is correct");
+    }else{
+        QMessageBox::warning(this,"Login","UserName and password is not correct");
+    }
+}
+
